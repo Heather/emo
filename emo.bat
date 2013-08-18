805b52b27b07560d304305506c654984646e5ff5
Heather <Heather@cynede.net>
2013-08-18 14:25:21 +0400

    fix batch file

--- emo.bat
+++ emo.bat
@@ -3,8 +3,12 @@ cls
 SET EnableNuGetPackageRestore=true
 if not exist "tools\emo\tools\emo.exe" (
 	"tools\nuget\nuget.exe" "install" "emo" "-OutputDirectory" "tools" "-ExcludeVersion"
-    if exist "%ProgramFiles%\Reference Assemblies\Microsoft\FSharp\.NETFramework\v4.0\4.3.0.0\FSharp.Core.dll" (
-        cp "%ProgramFiles%\Reference Assemblies\Microsoft\FSharp\.NETFramework\v4.0\4.3.0.0\FSharp.Core.dll" "tools\emo\tools\FSharp.Core.dll"
+    ::if exist "%ProgramFiles(x86)%\Reference Assemblies\Microsoft\FSharp\.NETFramework\v4.0\4.3.1.0\FSharp.Core.dll" (
+    ::    COPY "%ProgramFiles(x86)%\Reference Assemblies\Microsoft\FSharp\.NETFramework\v4.0\4.3.1.0\FSharp.Core.dll" "tools\emo\tools\FSharp.Core.dll"
+    ::)
+    ::else 
+    if exist "%ProgramFiles(x86)%\Reference Assemblies\Microsoft\FSharp\.NETFramework\v4.0\4.3.0.0\FSharp.Core.dll" (
+        COPY "%ProgramFiles(x86)%\Reference Assemblies\Microsoft\FSharp\.NETFramework\v4.0\4.3.0.0\FSharp.Core.dll" "tools\emo\tools\FSharp.Core.dll"
     )
 )
 "tools\emo\tools\emo.exe"
